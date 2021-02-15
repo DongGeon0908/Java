9c85a72d7d6caf6b4879df1ce3d1bfb78b544160
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-02-15 20:34:49 +0900

    @annotation

---
+++ 2021-02-15/src/Annotation/Service.java
@@ -0,0 +1,19 @@
+package Annotation;
+
+public class Service {
+
+	@PrintAnnotation
+	public void method1() {
+		System.out.println("실행 내용1");
+	}
+
+	@PrintAnnotation("*")
+	public void method2() {
+		System.out.println("실행 내용2");
+	}
+
+	@PrintAnnotation(value = "#", number = 20)
+	public void metho3() {
+		System.out.println("실행 내용3");
+	}
+}
