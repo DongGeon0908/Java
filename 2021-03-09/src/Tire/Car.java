5d97241fa5937a2e34cb8b3f189cc095d951a4ad
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-09 14:27:33 +0900

    polymorphism with inerface ArrayList

--- 2021-03-09/src/Tire/Car.java
+++ 2021-03-09/src/Tire/Car.java
@@ -1,16 +1,17 @@
 package Tire;
 
 public class Car {
-	Tire frontLeftTire = new HankookTire();
-	Tire frontRightTire = new HankookTire();
-	Tire backLeftTire = new HankookTire();
-	Tire backRightTire = new HankookTire();
+	Tire[] tires = {
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire()
+	};
 
 	void run() {
-		frontLeftTire.roll();
-		frontRightTire.roll();
-		backLeftTire.roll();
-		backRightTire.roll();
+		for(Tire tire : tires) {
+			tire.roll();
+		}
 	}
 
 }
