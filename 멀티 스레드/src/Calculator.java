778406d3d9f5637260022e302b8f5710113fcf0e
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-04-13 18:10:18 +0900

    동기화 메서드

--- 멀티 스레드/src/Calculator.java
+++ 멀티 스레드/src/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		return memory;
 	}
 
-	public void setMemory(int memory) {
+	public synchronized void setMemory(int memory) {
 		this.memory = memory;
 		try {
 			Thread.sleep(2000);
@@ -14,5 +14,18 @@ public class Calculator {
 
 		}
 		System.out.println(Thread.currentThread().getName() + " : " + this.memory);
+	}
+
+	// setMemory()와 동등한 역할
+	public void setMemory2(int memory) {
+		synchronized (this) {
+			this.memory = memory;
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+
+			}
+			System.out.println(Thread.currentThread().getName() + " : " + this.memory);
+		}
 	}
 }
